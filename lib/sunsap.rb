91aefb1cd38d94b13269ea7c672df6f420c89514
_lambda <keymint1498@gmail.com>
2012-12-16 08:07:28 -0500

    Passing test

--- lib/sunsap.rb
+++ lib/sunsap.rb
@@ -4,39 +4,14 @@ module Sunsap
 
   class Reporter
     def initialize(reportername)
-      mediator = Mediator.new
-      mess     = Participant.new("#{reportername}", mediator)
-      super mediator, mess
+      @queue = []
+      self.class.send :define_method, "send_#{reportername}_message" do |m|
+        @queue.push(m)
+      end
 
-      self.class.send :define_method, "send_#{reportername}_message" do |message|
-        mediator.notify "Sunsap", "#{message}"
+      def queue
+        @queue
       end
     end
-  end
-end
-
-class Mediator
-  def initialize
-    @participants = Hash.new
-  end
-
-  def register(participant)
-    @participants[participant.id] = participant
-  end
-
-  def notify(from, to, message)
-    to_participant = @participants[to]
-    to_participant.receive(from) unless to_participant.nil?
-  end
-end
-
-class Participant
-  def initialize(id, mediator)
-    super id, mediator
-    mediator.register(self)
-  end
-
-  def receive(from, message)
-    "#{message}"
   end
 end
--- spec/lib/sunsap.rb
+++ spec/lib/sunsap.rb
@@ -3,8 +3,7 @@ require "spec_helper"
 describe Sunsap do
   it "creates new reporters" do
     reporter = Sunsap::Reporter.new("test")
-    reader = Sunsap::Reader.new
     reporter.send_test_message "Success!"
-    reader.queue.should be ["Success!"]
+    reporter.queue.should eq ["Success!"]
   end
 end
