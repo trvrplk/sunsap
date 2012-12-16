45703ced92e3f06b5a2f6f031b99f351b72d0c5d
_lambda <keymint1498@gmail.com>
2012-12-15 19:52:08 -0500

    Failing code

--- lib/sunsap.rb
+++ lib/sunsap.rb
@@ -1,3 +1,42 @@
 module Sunsap
   VERSION = '0.1.0'
+  attr_accessor :reportername, :messages
+
+  class Reporter
+    def initialize(reportername)
+      mediator = Mediator.new
+      mess     = Participant.new("#{reportername}", mediator)
+      super mediator, mess
+
+      self.class.send :define_method, "send_#{reportername}_message" do |message|
+        mediator.notify "Sunsap", "#{message}"
+      end
+    end
+  end
+end
+
+class Mediator
+  def initialize
+    @participants = Hash.new
+  end
+
+  def register(participant)
+    @participants[participant.id] = participant
+  end
+
+  def notify(from, to, message)
+    to_participant = @participants[to]
+    to_participant.receive(from) unless to_participant.nil?
+  end
+end
+
+class Participant
+  def initialize(id, mediator)
+    super id, mediator
+    mediator.register(self)
+  end
+
+  def receive(from, message)
+    "#{message}"
+  end
 end
--- spec/lib/sunsap.rb
+++ spec/lib/sunsap.rb
@@ -1,9 +1,10 @@
 require "spec_helper"
 
-describe "Sunsap" do
+describe Sunsap do
   it "creates new reporters" do
-    reporter = Sunsap::Reporter.new(name: "test")
-    reporter.send(message: "Success!")
-    reporter.test_count.should be "1"
+    reporter = Sunsap::Reporter.new("test")
+    reader = Sunsap::Reader.new
+    reporter.send_test_message "Success!"
+    reader.queue.should be ["Success!"]
   end
 end
